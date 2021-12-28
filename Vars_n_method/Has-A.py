9c8c1f19e456c7677035116bb543e9e82503c757
purvapr13 <purvapr13@gmail.com>
2021-12-28 14:59:04 +0530

    Has-A Relationship Example

---
+++ Vars_n_method/Has-A.py
@@ -0,0 +1,19 @@
+#Example of Has-A relationship
+class veg:
+    def vegMenu(self):
+        print('Only Veg Pulao is available in veg')
+class nonveg:
+    def nonvegMenu(self):
+        print('only Chicken Biryani is available in non-veg')
+class restaurant:
+    def __init__(self):
+        self.veg = veg()
+        self.nonveg = nonveg()
+
+    def overallMenu(self):
+        print("welcome to Eatery, please look our menu here")
+        self.veg.vegMenu()
+        self.nonveg.nonvegMenu()
+
+visit = restaurant()
+visit.overallMenu()
